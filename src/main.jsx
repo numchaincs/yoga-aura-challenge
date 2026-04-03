e58ad277002c80fed0a6cd609c7a90e0f4866b94
numchaincs <numchai.nc@gmail.com>
2026-04-03 20:26:25 +0700

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+// 1. เพิ่มบรรทัดนี้เพื่อ import ตัวแทร็ก
+import { Analytics } from "@vercel/analytics/react"; 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <App />
+    {/* 2. วางตัวแทร็กไว้ตรงนี้ข้างๆ <App /> */}
+    <Analytics /> 
+  </React.StrictMode>
 );
